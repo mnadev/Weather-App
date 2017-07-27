772da2815e3dfd4c1ee2339642d143f009202734
mnadev <mohammednadeem902@gmail.com>
2017-07-27 17:08:39 -0400

    Cleaned up code

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ $(document).ready( function() {
 			console.log(weather);
 			var dateObj = new Date(); //create new date object
 			var monthArr = ["January", "February" , "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-			var currHour = dateObj.getHours() % 12;
+			var currHour = dateObj.getHours() % 12; //current hour
 			var date = currHour + ":" + dateObj.getMinutes() + ",     " + monthArr[dateObj.getMonth()] + " " + dateObj.getDate(); //get hours, min, day, month
 			var isFahrenheit = true; //create variable to check if temp is in fahrenheit or celcius
 
@@ -25,32 +25,18 @@ $(document).ready( function() {
 			var mainDiv = document.getElementById("main"); // get div of id main
 
 			$("#temp").text(temp + " °F");
-			//var tempDisp = document.getElementById("temp"); //access element to display temp
-			//tempDisp.innerHTML = temp + " °F"; //display temp
 
 			$('#date').text(date);
-			//var dateDisp = document.getElementById("date"); //access element to display date
-			//dateDisp.innerHTML = date; //add date to html
 
 			$('#weather').html("The weather is " + weather);
-			//var weatherDisp = document.getElementById("weather"); //access element to display weather
-			//weatherDisp.innerHTML = weather; //add weather text to div
 
 			$("#humidity").text(humidity + "% humidity");
-			//var humidDisp = document.getElementById("humidity"); //access element to display humidity
-			//humidDisp.innerHTML = humidity + "%"; //add humidity text to html
 
 			$("#precip").text(precipChance + "% precipitation chance");
-			//var precipDisp = document.getElementById("precip"); //access element to display precipitation
-			//precipDisp.innerHTML = precipChance + "%"; //add precipitation chance to html
 
 			$("#celcius").text("°C");
-			//var btnCelcius = document.getElementById("celcius"); //access button to change temp to celcius
-			//btnCelcius.innerHTML = "°C";//adding text showing celcius
 
 			$("#fahr").text("°F");
-			//var btnFahr = document.getElementById("fahr"); //access button to change temp to fahrenheit
-			//btnFahr.innerHTML = "°F";//adding text showing fahrenheit
 
 			$("#celcius").click(function(event) {
 				if(isFahrenheit) { //check if temp is in degrees fahrenheit
@@ -60,12 +46,6 @@ $(document).ready( function() {
 					isFahrenheit = false;
 				}
 			});
-			//btnCelcius.addEventListener("click", function(event) { //change temp if button clicked
-				//if(isFahrenheit) { //check if temp is in degrees fahrenheit
-					//temp = (temp - 32) * (5/9); //convert function to celcius from fahrenheit
-					//isFahrenheit = false;
-				//}
-			//});
 
 			$("#fahr").click(function(event) {
 				if(!isFahrenheit) { //check if temp is in degrees fahrenheit
@@ -76,16 +56,7 @@ $(document).ready( function() {
 				}
 			});
 
-			//btnFahr.addEventListener("click", function(event) { //change temp if button clicked
-				//if(!isFahrenheit) { //check if temp is already fahrenheit
-					//temp = (temp * 9/5) + 32; //convert function from celcius to fahrenheit
-					//isFahrenheit = true;
-				//}
-			//});
-
 			$("#hourly").text("Show next 6 hours:");
-			//var btnHour = document.getElementById("hourly"); //creating button to show hourly weather
-			//btnHour.innerHTML = "Hourly Weather";
 
 			$("#hourly").click(function(event) {
 				var hourlyData = jsonWeather.hourly.data;
@@ -95,7 +66,7 @@ $(document).ready( function() {
 				for(var i = 1; i < 7; i++) { //iterate through each hour, getting data
 					hourlyTemps[i] = hourlyData[i].temperature;
 					hourlySummary[i] = hourlyData[i].summary;
-					var classHr = ".hour" + i;
+					var classHr = ".hour" + i; //html classes for each hour
 					var classTemp = ".temp" + i;
 					var classSumm = ".summary" + i;
 
@@ -108,16 +79,6 @@ $(document).ready( function() {
 
 				$("#hr").show();
 			});
-
-			//btnHour.addEventListener("click", function(event) {//event listener for hourly weather button
-				//var hourlyData = data.hourly.data;
-				//var hourlyTemps = []; //array storing temperate each hour
-
-				//for(var i = 0; i < hourlyData.length; i++) { //iterate through each hour, getting data
-					//alert(hourlyData[i]);
-				//}
-
-			//});
 
 		});
 	});
